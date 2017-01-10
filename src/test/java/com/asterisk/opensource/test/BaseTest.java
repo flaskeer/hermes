ee077bf6cbd886f74e869c725398bcda0359dba8
flaskeer <1185098948@qq.com>
2017-01-10 22:19:12 +0800

    docker file

--- src/test/java/com/asterisk/opensource/test/BaseTest.java
+++ src/test/java/com/asterisk/opensource/test/BaseTest.java
@@ -1,5 +1,13 @@
 package com.asterisk.opensource.test;
 
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class BaseTest {
+
+
 }
