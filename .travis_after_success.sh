a578f09e8293510d0f76f5809025fb545d4bc60c
dongh38 <Dh13623652058>
2017-04-06 11:34:56 +0800

    add travis

---
+++ .travis_after_success.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [[ "${TRAVIS_JDK_VERSION}" != "oraclejdk8" ]]; then
+    echo "Skipping after_success actions for JDK version \"${TRAVIS_JDK_VERSION}\""
+    exit
+fi
+
+mvn clean test jacoco:report coveralls:report
